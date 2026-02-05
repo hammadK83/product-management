4f0d69942166f6944672acfeb9f27ab5c0ee3ac3
Hammad Khalid <hammadhsnkh@gmail.com>
2026-02-05 13:31:08 -0600

    Fix typo

--- src/lambda/products/createProduct.ts
+++ src/lambda/products/createProduct.ts
@@ -1,8 +1,8 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
 
-export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> {
+export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
     return {
-        statusCode: 201,
+        statusCode: 20,
         body: JSON.stringify({ message: 'create product' }),
     }
 }
